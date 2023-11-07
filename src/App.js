c531cf0d8a276af75a80f7c3825b77d27b9f38fb
nitin <nkmrt30032003@gmail.com>
2023-11-07 23:15:10 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import Calculator from './component/age';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <Calculator />
+    </div>
+    
+  );
+}
+
+export default App;
+
